40f703e5cc108a0daf34fb9dd1e9d04e4f84882f
gnimas <csamsinges@gmail.com>
2022-09-07 20:07:38 +0200

    testing

---
+++ src/test/java/com/example/springjpaassignment/SpringJpaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpaassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
